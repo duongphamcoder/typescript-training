f42739d023c1c42213a1ffb7c27515b7fac036d3
Duong <phamtanduongtk@gmail.com>
2022-11-28 17:19:54 +0700

    #5 Update code handle add to-do

--- practices/practice-one/src/scripts/views/todo-form.view.ts
+++ practices/practice-one/src/scripts/views/todo-form.view.ts
@@ -7,34 +7,52 @@ export default class TodoFormView {
     private checkAllElement: HTMLButtonElement;
     private todosElement: HTMLUListElement;
     private textElment: HTMLInputElement
-    private count: number
+    private todos: Array<TodoType>
 
     constructor() {
+        const todoLocals = JSON.parse(localStorage.getItem('todos'))
+        this.todos = todoLocals || []
         this.formElement = querySelector('.form-submit form') as HTMLFormElement;
         this.checkAllElement = querySelector('.btn-tick') as HTMLButtonElement;
         this.todosElement = querySelector('.todos') as HTMLUListElement;
         this.textElment = querySelector('.form-submit  form > input') as HTMLInputElement;
-        this.count = 0;
     }
 
     init() {
+        this.render()
         this.addEventForm()
     }
 
+    private render() {
+        const todosElement = querySelector('.form')
+        this.todos.forEach(todo => {
+            const param: Param = {
+                data: todo,
+                handleCompletedTodo: this.handleCompletedTodo,
+                handleDeletedTodo: this.handleDeletedTodo,
+                handleUpdateTodo: this.handleUpdateTodo,
+            }
+            const todoItem = todoItemTemplate(param)
+            this.todosElement.appendChild(todoItem)
+            todosElement.classList.add('not-empty')
+        })
+    }
+
     private addEventForm() {
-        const todos = querySelector('.form')
+        const todosElement = querySelector('.form')
         this.formElement.addEventListener('submit', (e) => {
             e.preventDefault();
+            let count = this.todos.length
             const text = this.textElment.value
             if (text) {
                 const data: TodoType = {
-                    id: this.count++,
+                    id: count,
                     description: text,
                     isCompleted: false
                 }
                 this.handleAddTodo(data)
                 this.textElment.value = ''
-                todos.classList.add('not-empty')
+                todosElement.classList.add('not-empty')
             }
 
         })
@@ -43,6 +61,8 @@ export default class TodoFormView {
 
 
     private handleAddTodo(data: TodoType) {
+        this.todos.push(data)
+        localStorage.setItem('todos', JSON.stringify(this.todos))
         const param: Param = {
             data,
             handleCompletedTodo: this.handleCompletedTodo,
